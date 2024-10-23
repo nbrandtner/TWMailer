6bcc86d18df0469d82d723f96d3cf7bb4eb7ed58
nbrandtner-tgm <nbrandtner@gmx.at>
2024-10-23 20:42:26 +0200

    add implementation from other directory

--- ClientServerSample/myclient.c
+++ ClientServerSample/myclient.c
@@ -7,174 +7,201 @@
 #include <stdio.h>
 #include <string.h>
 
-///////////////////////////////////////////////////////////////////////////////
-
 #define BUF 1024
-#define PORT 6543
 
-///////////////////////////////////////////////////////////////////////////////
+void sendCommand(int socket, const char *command);
+void handleSend(int socket);
+void handleList(int socket);
+void handleRead(int socket);
+void handleDel(int socket);
 
 int main(int argc, char **argv)
 {
-   int create_socket;
-   char buffer[BUF];
-   struct sockaddr_in address;
-   int size;
-   int isQuit;
+   if (argc != 3)
+   {
+      printf("Usage: %s <ip> <port>\n", argv[0]);
+      return EXIT_FAILURE;
+   }
 
-   ////////////////////////////////////////////////////////////////////////////
-   // CREATE A SOCKET
-   // https://man7.org/linux/man-pages/man2/socket.2.html
-   // https://man7.org/linux/man-pages/man7/ip.7.html
-   // https://man7.org/linux/man-pages/man7/tcp.7.html
-   // IPv4, TCP (connection oriented), IP (same as server)
+   int create_socket;
+   struct sockaddr_in address;
+   char buffer[BUF];
+
+   // Create socket
    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
    {
       perror("Socket error");
       return EXIT_FAILURE;
    }
 
-   ////////////////////////////////////////////////////////////////////////////
-   // INIT ADDRESS
-   // Attention: network byte order => big endian
-   memset(&address, 0, sizeof(address)); // init storage with 0
-   address.sin_family = AF_INET;         // IPv4
-   // https://man7.org/linux/man-pages/man3/htons.3.html
-   address.sin_port = htons(PORT);
-   // https://man7.org/linux/man-pages/man3/inet_aton.3.html
-   if (argc < 2)
+   // Set server address
+   address.sin_family = AF_INET;
+   address.sin_port = htons(atoi(argv[2]));
+   if (inet_pton(AF_INET, argv[1], &address.sin_addr) <= 0)
    {
-      inet_aton("127.0.0.1", &address.sin_addr);
-   }
-   else
-   {
-      inet_aton(argv[1], &address.sin_addr);
-   }
-
-   ////////////////////////////////////////////////////////////////////////////
-   // CREATE A CONNECTION
-   // https://man7.org/linux/man-pages/man2/connect.2.html
-   if (connect(create_socket,
-               (struct sockaddr *)&address,
-               sizeof(address)) == -1)
-   {
-      // https://man7.org/linux/man-pages/man3/perror.3.html
-      perror("Connect error - no server available");
+      perror("Address conversion error");
       return EXIT_FAILURE;
    }
 
-   // ignore return value of printf
-   printf("Connection with server (%s) established\n",
-          inet_ntoa(address.sin_addr));
-
-   ////////////////////////////////////////////////////////////////////////////
-   // RECEIVE DATA
-   // https://man7.org/linux/man-pages/man2/recv.2.html
-   size = recv(create_socket, buffer, BUF - 1, 0);
-   if (size == -1)
+   // Connect to server
+   if (connect(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1)
    {
-      perror("recv error");
-   }
-   else if (size == 0)
-   {
-      printf("Server closed remote socket\n"); // ignore error
-   }
-   else
-   {
-      buffer[size] = '\0';
-      printf("%s", buffer); // ignore error
+      perror("Connect error");
+      return EXIT_FAILURE;
    }
 
-   do
+   int running = 1;
+   while (running)
    {
-      printf(">> ");
-      if (fgets(buffer, BUF - 1, stdin) != NULL)
+      printf("Choose a command: SEND, LIST, READ, DEL, QUIT\n");
+      fgets(buffer, BUF, stdin);
+      buffer[strcspn(buffer, "\n")] = 0;
+
+      if (strcmp(buffer, "SEND") == 0)
       {
-         int size = strlen(buffer);
-         // remove new-line signs from string at the end
-         if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
-         {
-            size -= 2;
-            buffer[size] = 0;
-         }
-         else if (buffer[size - 1] == '\n')
-         {
-            --size;
-            buffer[size] = 0;
-         }
-         isQuit = strcmp(buffer, "quit") == 0;
-
-         //////////////////////////////////////////////////////////////////////
-         // SEND DATA
-         // https://man7.org/linux/man-pages/man2/send.2.html
-         // send will fail if connection is closed, but does not set
-         // the error of send, but still the count of bytes sent
-         if ((send(create_socket, buffer, size + 1, 0)) == -1) 
-         {
-            // in case the server is gone offline we will still not enter
-            // this part of code: see docs: https://linux.die.net/man/3/send
-            // >> Successful completion of a call to send() does not guarantee 
-            // >> delivery of the message. A return value of -1 indicates only 
-            // >> locally-detected errors.
-            // ... but
-            // to check the connection before send is sense-less because
-            // after checking the communication can fail (so we would need
-            // to have 1 atomic operation to check...)
-            perror("send error");
-            break;
-         }
-
-         //////////////////////////////////////////////////////////////////////
-         // RECEIVE FEEDBACK
-         // consider: reconnect handling might be appropriate in somes cases
-         //           How can we determine that the command sent was received 
-         //           or not? 
-         //           - Resend, might change state too often. 
-         //           - Else a command might have been lost.
-         //
-         // solution 1: adding meta-data (unique command id) and check on the
-         //             server if already processed.
-         // solution 2: add an infrastructure component for messaging (broker)
-         //
-         size = recv(create_socket, buffer, BUF - 1, 0);
-         if (size == -1)
-         {
-            perror("recv error");
-            break;
-         }
-         else if (size == 0)
-         {
-            printf("Server closed remote socket\n"); // ignore error
-            break;
-         }
-         else
-         {
-            buffer[size] = '\0';
-            printf("<< %s\n", buffer); // ignore error
-            if (strcmp("OK", buffer) != 0)
-            {
-               fprintf(stderr, "<< Server error occured, abort\n");
-               break;
-            }
-         }
+         handleSend(create_socket);
       }
-   } while (!isQuit);
-
-   ////////////////////////////////////////////////////////////////////////////
-   // CLOSES THE DESCRIPTOR
-   if (create_socket != -1)
-   {
-      if (shutdown(create_socket, SHUT_RDWR) == -1)
+      else if (strcmp(buffer, "LIST") == 0)
       {
-         // invalid in case the server is gone already
-         perror("shutdown create_socket"); 
+         handleList(create_socket);
       }
-      if (close(create_socket) == -1)
+      else if (strcmp(buffer, "READ") == 0)
       {
-         perror("close create_socket");
+         handleRead(create_socket);
       }
-      create_socket = -1;
+      else if (strcmp(buffer, "DEL") == 0)
+      {
+         handleDel(create_socket);
+      }
+      else if (strcmp(buffer, "QUIT") == 0)
+      {
+         sendCommand(create_socket, "QUIT\n");
+         running = 0;
+      }
+      else
+      {
+         printf("Unknown command. Please try again.\n");
+      }
    }
 
+   close(create_socket);
    return EXIT_SUCCESS;
+}
+
+void sendCommand(int socket, const char *command)
+{
+   if (send(socket, command, strlen(command), 0) == -1)
+   {
+      perror("Send error");
+   }
+}
+
+void handleSend(int socket)
+{
+   char sender[9], receiver[9], subject[81], message[BUF];
+
+   printf("Enter sender (max 8 chars): ");
+   fgets(sender, sizeof(sender), stdin);
+   sender[strcspn(sender, "\n")] = 0;
+
+   printf("Enter receiver (max 8 chars): ");
+   fgets(receiver, sizeof(receiver), stdin);
+   receiver[strcspn(receiver, "\n")] = 0;
+
+   printf("Enter subject (max 80 chars): ");
+   fgets(subject, sizeof(subject), stdin);
+   subject[strcspn(subject, "\n")] = 0;
+
+   printf("Enter message (end with a single dot on a line):\n");
+   memset(message, 0, BUF);
+   char line[BUF];
+   while (fgets(line, BUF, stdin))
+   {
+      if (strcmp(line, ".\n") == 0)
+      {
+         break;
+      }
+      strcat(message, line);
+   }
+
+   char command[BUF];
+   snprintf(command, BUF, "SEND\n%s\n%s\n%s\n%s.\n", sender, receiver, subject, message);
+   sendCommand(socket, command);
+
+   char response[BUF];
+   memset(response, 0, BUF);
+   if (recv(socket, response, BUF, 0) > 0)
+   {
+      printf("Server response: %s", response);
+   }
+}
+
+void handleList(int socket)
+{
+   char username[9];
+
+   printf("Enter username (max 8 chars): ");
+   fgets(username, sizeof(username), stdin);
+   username[strcspn(username, "\n")] = 0;
+
+   char command[BUF];
+   snprintf(command, BUF, "LIST\n%s\n", username);
+   sendCommand(socket, command);
+
+   char response[BUF];
+   memset(response, 0, BUF);
+   if (recv(socket, response, BUF, 0) > 0)
+   {
+      printf("Server response: %s", response);
+   }
+}
+
+void handleRead(int socket)
+{
+   char username[9];
+   char messageNumber[BUF];
+
+   printf("Enter username (max 8 chars): ");
+   fgets(username, sizeof(username), stdin);
+   username[strcspn(username, "\n")] = 0;
+
+   printf("Enter message number: ");
+   fgets(messageNumber, sizeof(messageNumber), stdin);
+   messageNumber[strcspn(messageNumber, "\n")] = 0;
+
+   char command[BUF];
+   snprintf(command, BUF, "READ\n%s\n%s\n", username, messageNumber);
+   sendCommand(socket, command);
+
+   char response[BUF];
+   memset(response, 0, BUF);
+   if (recv(socket, response, BUF, 0) > 0)
+   {
+      printf("Server response: %s", response);
+   }
+}
+
+void handleDel(int socket)
+{
+   char username[9];
+   char messageNumber[BUF];
+
+   printf("Enter username (max 8 chars): ");
+   fgets(username, sizeof(username), stdin);
+   username[strcspn(username, "\n")] = 0;
+
+   printf("Enter message number: ");
+   fgets(messageNumber, sizeof(messageNumber), stdin);
+   messageNumber[strcspn(messageNumber, "\n")] = 0;
+
+   char command[BUF];
+   snprintf(command, BUF, "DEL\n%s\n%s\n", username, messageNumber);
+   sendCommand(socket, command);
+
+   char response[BUF];
+   memset(response, 0, BUF);
+   if (recv(socket, response, BUF, 0) > 0)
+   {
+      printf("Server response: %s", response);
+   }
 }
--- ClientServerSample/myserver.c
+++ ClientServerSample/myserver.c
@@ -156,6 +156,273 @@ int main(void)
 
    return EXIT_SUCCESS;
 }
+void signalHandler(int sig)
+{
+   if (sig == SIGINT)
+   {
+      printf("\nSIGINT received, shutting down server...\n");
+      abortRequested = 1;
+      if (create_socket != -1)
+         close(create_socket);
+      if (new_socket != -1)
+         close(new_socket);
+   }
+}
+
+void handleSendCommand(int client_socket)
+{
+   char sender[9], receiver[9], subject[81], message[BUF];
+   memset(sender, 0, sizeof(sender));
+   memset(receiver, 0, sizeof(receiver));
+   memset(subject, 0, sizeof(subject));
+   memset(message, 0, sizeof(message));
+
+   // Receive sender
+   if (recv(client_socket, sender, 8, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Receive receiver
+   if (recv(client_socket, receiver, 8, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Receive subject
+   if (recv(client_socket, subject, 80, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Receive message (until ".\n" is found)
+   char buffer[BUF];
+   while (1)
+   {
+      memset(buffer, 0, BUF);
+      int bytesReceived = recv(client_socket, buffer, BUF, 0);
+      if (bytesReceived <= 0)
+      {
+         send(client_socket, "ERR\n", 4, 0);
+         return;
+      }
+      strcat(message, buffer);
+      if (strstr(buffer, "\.\n"))
+         break;
+   }
+
+   // Store message in mail spool directory (e.g., "mail-spool/<receiver>")
+   char directory[BUF];
+   snprintf(directory, BUF, "mail-spool/%s", receiver);
+   mkdir(directory, 0777);
+
+   char filepath[BUF];
+   snprintf(filepath, BUF, "%s/message.txt", directory);
+   int fd = open(filepath, O_CREAT | O_WRONLY | O_APPEND, 0666);
+   if (fd == -1)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   dprintf(fd, "From: %s\nTo: %s\nSubject: %s\nMessage:\n%s\n\n", sender, receiver, subject, message);
+   close(fd);
+
+   send(client_socket, "OK\n", 3, 0);
+}
+
+void handleListCommand(int client_socket)
+{
+   char username[9];
+   memset(username, 0, sizeof(username));
+
+   // Receive username
+   if (recv(client_socket, username, 8, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Open user directory
+   char directory[BUF];
+   snprintf(directory, BUF, "mail-spool/%s", username);
+   DIR *dir = opendir(directory);
+   if (dir == NULL)
+   {
+      send(client_socket, "0\n", 3, 0);
+      return;
+   }
+
+   // List messages
+   struct dirent *entry;
+   int messageCount = 0;
+   char response[BUF] = {0};
+
+   while ((entry = readdir(dir)) != NULL)
+   {
+      if (entry->d_type == DT_REG)
+      {
+         messageCount++;
+         strcat(response, entry->d_name);
+         strcat(response, "\n");
+      }
+   }
+
+   closedir(dir);
+
+   char countStr[16];
+   snprintf(countStr, sizeof(countStr), "%d\n", messageCount);
+   send(client_socket, countStr, strlen(countStr), 0);
+   send(client_socket, response, strlen(response), 0);
+}
+
+void handleReadCommand(int client_socket)
+{
+   char username[9];
+   int messageNumber;
+   memset(username, 0, sizeof(username));
+
+   // Receive username
+   if (recv(client_socket, username, 8, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Receive message number
+   char buffer[BUF];
+   memset(buffer, 0, BUF);
+   if (recv(client_socket, buffer, BUF, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+   messageNumber = atoi(buffer);
+
+   // Open user directory
+   char directory[BUF];
+   snprintf(directory, BUF, "mail-spool/%s", username);
+   DIR *dir = opendir(directory);
+   if (dir == NULL)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Find the specified message
+   struct dirent *entry;
+   int currentMessage = 0;
+   char filepath[BUF] = {0};
+
+   while ((entry = readdir(dir)) != NULL)
+   {
+      if (entry->d_type == DT_REG)
+      {
+         if (currentMessage == messageNumber)
+         {
+            snprintf(filepath, BUF, "%s/%s", directory, entry->d_name);
+            break;
+         }
+         currentMessage++;
+      }
+   }
+
+   closedir(dir);
+
+   if (strlen(filepath) == 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Read and send the message content
+   int fd = open(filepath, O_RDONLY);
+   if (fd == -1)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   memset(buffer, 0, BUF);
+   read(fd, buffer, BUF);
+   close(fd);
+
+   send(client_socket, "OK\n", 3, 0);
+   send(client_socket, buffer, strlen(buffer), 0);
+}
+
+void handleDelCommand(int client_socket)
+{
+   char username[9];
+   int messageNumber;
+   memset(username, 0, sizeof(username));
+
+   // Receive username
+   if (recv(client_socket, username, 8, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Receive message number
+   char buffer[BUF];
+   memset(buffer, 0, BUF);
+   if (recv(client_socket, buffer, BUF, 0) <= 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+   messageNumber = atoi(buffer);
+
+   // Open user directory
+   char directory[BUF];
+   snprintf(directory, BUF, "mail-spool/%s", username);
+   DIR *dir = opendir(directory);
+   if (dir == NULL)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Find the specified message
+   struct dirent *entry;
+   int currentMessage = 0;
+   char filepath[BUF] = {0};
+
+   while ((entry = readdir(dir)) != NULL)
+   {
+      if (entry->d_type == DT_REG)
+      {
+         if (currentMessage == messageNumber)
+         {
+            snprintf(filepath, BUF, "%s/%s", directory, entry->d_name);
+            break;
+         }
+         currentMessage++;
+      }
+   }
+
+   closedir(dir);
+
+   if (strlen(filepath) == 0)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   // Delete the message
+   if (unlink(filepath) == -1)
+   {
+      send(client_socket, "ERR\n", 4, 0);
+      return;
+   }
+
+   send(client_socket, "OK\n", 3, 0);
+}
+
 
 void *clientCommunication(void *data)
 {
